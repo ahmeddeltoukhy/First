6f809bdd0b470915be5db7ae00e4a2480a5cc1fd
Ahmed Eltoukhy <ahmedeltoukhy.ai@gmail.com>
2022-09-20 21:23:37 +0200

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Ahmed Eltoukhy on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
