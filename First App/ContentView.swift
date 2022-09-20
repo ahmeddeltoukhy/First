543267be99017c1d2df699426bef0da76002d55b
Ahmed Eltoukhy <ahmedeltoukhy.ai@gmail.com>
2022-09-20 21:48:23 +0200

    abou

--- First App/ContentView.swift
+++ First App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Eltoukhy")
+            Text("a7a")
         }
         .padding()
     }
